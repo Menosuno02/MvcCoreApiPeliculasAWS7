4c6d9c814a50da5243040f27fdef1d5733f43fe3
Alejandro López Labajo <mainusone1@gmail.com>
2024-05-29 10:25:53 +0200

    Commit

--- MvcCoreApiPeliculasAWS7/Controllers/PeliculasController.cs
+++ MvcCoreApiPeliculasAWS7/Controllers/PeliculasController.cs
@@ -7,10 +7,12 @@ namespace MvcCoreApiPeliculasAWS7.Controllers
     public class PeliculasController : Controller
     {
         private ServiceApiPeliculas service;
+        private ServiceStorageAWS serviceStorage;
 
-        public PeliculasController(ServiceApiPeliculas service)
+        public PeliculasController(ServiceApiPeliculas service, ServiceStorageAWS serviceStorage)
         {
             this.service = service;
+            this.serviceStorage = serviceStorage;
         }
 
         public async Task<IActionResult> Index()
@@ -28,10 +30,10 @@ namespace MvcCoreApiPeliculasAWS7.Controllers
         [HttpPost]
         public async Task<IActionResult> PeliculasActor(string actor)
         {
-                List<Pelicula> peliculas =
-                    await this.service
-                    .GetPeliculasActoresAsync(actor);
-                return View(peliculas);
+            List<Pelicula> peliculas =
+                await this.service
+                .GetPeliculasActoresAsync(actor);
+            return View(peliculas);
         }
 
         public async Task<IActionResult> Details(int id)
@@ -46,8 +48,13 @@ namespace MvcCoreApiPeliculasAWS7.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Pelicula pelicula)
+        public async Task<IActionResult> Create(Pelicula pelicula, IFormFile file)
         {
+            pelicula.Foto = file.FileName;
+            using (Stream stream = file.OpenReadStream())
+            {
+                await this.serviceStorage.UploadFileAsync(file.FileName, stream);
+            }
             await this.service.CreatePeliculaAsync(pelicula);
             return RedirectToAction("Index");
         }
